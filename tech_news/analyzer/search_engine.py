e59df42211e615b9a614cd1fae0d61820de2a63d
Beatriz Vago <beatriz@Rafaels-MBP.Home>
2023-11-27 16:35:56 +0000

    Req 07, 08 e 09

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,47 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    result = []
+    query = {"title": {"$regex": title, "$options": "i"}}
+    news = search_news(query)
+
+    for new in news:
+        news_list_item = (new["title"], new["url"])
+        result.append(news_list_item)
+
+    return result
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    # datetime.strptime(date_string, format)
+    try:
+        iso_format = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    result = []
+    query = {"timestamp": iso_format}
+    news = search_news(query)
+
+    for new in news:
+        news_list_item = (new["title"], new["url"])
+        result.append(news_list_item)
+
+    return result
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    result = []
+    query = {"category": {"$regex": category, "$options": "i"}}
+    news = search_news(query)
+
+    for new in news:
+        news_list_item = (new["title"], new["url"])
+        result.append(news_list_item)
+
+    return result
